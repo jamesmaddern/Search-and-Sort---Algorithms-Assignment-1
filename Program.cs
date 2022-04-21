b41d2440c09420ee05d4234de0d4b225dfd0b616
James Maddern <JamesMaddern@live.co.uk>
2022-04-21 09:49:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        /*Program program = new Program();
+
+        string path = Environment.CurrentDirectory;
+        path = Directory.GetParent(path).FullName;
+        string file = Path.Combine(path, "hello.txt");
+        program.write(file);
+        */
+        Reader r = new Reader();
+        foreach(string[] f in r.fileContent)
+        {
+            foreach(string item in f)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+
+    /*public async void write(string file)
+    {
+        await File.WriteAllTextAsync(file, "hello there");
+    }*/
+
+    
+}
---
+++ Reader.cs
@@ -0,0 +1,35 @@
+﻿internal class Reader
+{
+    public string path;
+    List<string> files = new List<string>();
+    public List<string[]> fileContent = new List<string[]>();
+
+    public Reader()
+    {
+        Directory.SetCurrentDirectory(@"..\..\..\");
+        path = Directory.GetCurrentDirectory();
+        getFileNames();
+        readFiles();
+
+    }
+    private void getFileNames()
+    {
+        int i = -1;
+        foreach(string filename in Directory.GetFiles(path))
+        {
+            if (filename.EndsWith(".txt"))
+            {
+                files.Add(filename);
+            }
+        }
+        Console.WriteLine(files);
+    }
+    private void readFiles()
+    {
+        for(int i = 0; i < files.Count; i++)
+        {
+            string filename = files[i];
+            fileContent.Add(File.ReadAllLines(filename));
+        }
+    }
+}
